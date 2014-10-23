8857d59a18dafce55b4f7d0998492390a7d1a9e6
Erik Quenon Steggall <esteggal@ucsc.edu>
2014-10-23 16:19:03 -0700

    Updating perceptron to *think* is correct

--- ml_hw2/perceptron.py
+++ ml_hw2/perceptron.py
@@ -1,23 +1,31 @@
 #!/usr/bin/python
 
 import random
+import numpy as np
 
 def preceptron_algo():
     my_inst_array = generate_instances_list()
-    w = 0
+    w_array = [0,0,0,0,0,0,0,0,0,0,0]
     y = 0
     x = 0
     b = 0
+    mistakes = 0
     for i in range(0,500):
         my_inst = my_inst_array[i]
         y = my_inst[0]
-        for j in range(0,11):
-            x = my_inst[1][j]
-            a = w*x + b
-            if a*y <= 0:
-                w = w + x*y
-            print "a =", a
-            print "w =", w
+        a = np.dot(w_array, my_inst[1])
+        if (y*a <= 0):
+            print "updating"
+            w_array = w_array + np.dot(y, my_inst[1])
+            print "w_array = ", w_array
+
+#        for j in range(0,11):
+#            x = my_inst[1][j]
+#            w = w_array[j]
+#            a = w*x + b
+#            if (y*a <= 0):
+#                w_array[j] += w + x*y
+#                mistakes += 1
 
 def generate_instances_list():
     # create an array to hold each of the 500 instance
