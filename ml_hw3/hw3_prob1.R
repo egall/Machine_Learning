ec2d5d7d9c53894634867de07f3e41e8c215d6e0
Erik Quenon Steggall <esteggal@ucsc.edu>
2014-10-28 17:11:58 -0700

    Small additions to the R file for problem 1

--- ml_hw3/hw3_prob1.R
+++ ml_hw3/hw3_prob1.R
@@ -7,10 +7,28 @@ cat("diff =", diff, "\n")
 meana <- mean(a)
 meanb <- mean(b)
 
+sda <- sd(a)
+sdb <- sd(b)
+sddiff <- sd(diff)
+
+cat("standard deviation of a = ", sda, "standard deviation of b = ", sdb, "standard deviation of diff = ", sddiff, "\n")
+
 cat("mean of a =", meana, "mean of b =", meanb, "\n")
 
 meandiff <- meanb-meana
 
+
+
 cat("difference of means = ", meandiff, "\n")
 
-print(t.test(b, a, mu = 0, paired=T, alternative="greater"))
+print(t.test(b, a, mu = meandiff, paired=T, alternative="greater"))
+
+
+#t_test = t.test(b, a, mu = meandiff, paired=T, alternative="greater")
+#t_test = t.test(b, a, paired=T)
+t_test = t.test(b, a, paired=T, alternative="greater")
+
+print(names(t_test))
+
+# print out the t-statistic
+print(t_test$statistic)
