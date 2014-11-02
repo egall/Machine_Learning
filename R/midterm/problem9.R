8d615385b0f6e03b90ddb07c48938570e1cd2d05
Erik Quenon Steggall <esteggal@ucsc.edu>
2014-11-01 20:22:55 -0700

    Finished problem 9

--- R/midterm/problem9.R
+++ R/midterm/problem9.R
@@ -1,9 +1,24 @@
 library(ISLR)
+library(boot)
 #print(summary(Default))
 
 attach(Default)
-
+# Part a)
 glm_info = glm(default ~ balance + income, data = Default, family=binomial)
 #print(summary(glm_info))
 
-print(summary(Default))
+# Part b)
+boot.fn=function(dat,index){
+    return(coef(glm(default ~ balance + income, data = dat, subset=index, family=binomial)))
+}
+
+#print(boot.fn(Default, 1:1000))
+
+#print(boot.fn(Default,sample(392,392,replace=T)))
+# Part c)
+
+set.seed(1)
+print(boot(Default, boot.fn, 1000))
+
+
+#print(summary(Default))
