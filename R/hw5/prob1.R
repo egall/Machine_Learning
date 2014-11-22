12d927a3fa065f57e484e8802cc0f5fa184e4f6d
Erik Quenon Steggall <esteggal@ucsc.edu>
2014-11-21 18:17:59 -0800

    First plot done

--- R/hw5/prob1.R
+++ R/hw5/prob1.R
@@ -2,6 +2,7 @@
 library(leaps)
 
 # Part a
+set.seed(1)
 X = rnorm(100)
 e = rnorm(100)
 
@@ -10,4 +11,17 @@ Y = 4 + 2*X + 4*X^2 + 8*X^3 + e
 print(Y)
 
 # Part c
-#regfit_full = regsubsets(
+xydata = data.frame(y = Y, x = X)
+regfit_full = regsubsets(y ~ poly(x, 10, raw=T), data = xydata, nvmax = 10) 
+reg_summary = summary(regfit_full)
+cat("Bic = ")
+print(which.min(reg_summary$bic))
+cat("cp = ")
+print(which.min(reg_summary$cp))
+cat("rsq = ")
+print(which.max(reg_summary$adjr2))
+
+pdf("BIC.pdf")
+plot(reg_summary$adjr2, xlab = "Number of Variables", ylab = "Adjusted RSq")
+points (4, reg_summary$adjr2[4], col="red", cex=2, pch =20)
+dev.off()
